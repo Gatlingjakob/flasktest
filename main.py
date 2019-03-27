a628bab33fa0ec69d07ad851f227593447716f93
Jakob Høy <gatlingjakob@hotmail.com>
2019-03-27 16:53:44 +0100

    added html

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from flask import Flask, render_template
+import mysql.connector
+import pandas as pd
+
+mydb = mysql.connector.connect(
+	  host="localhost",
+	  user="root",
+	  passwd="password",
+	  database="flaskapp"
+	)
+
+mycursor = mydb.cursor()
+
+df = pd.read_sql('SELECT * FROM BettingData WHERE Time >= NOW() - INTERVAL 8 HOUR', con=mydb)
+df = df[["HT_Name", "AT_Name", "HT_Score", "AT_Score", "MatchTime", "HT_Wins", "Draw", "AT_Wins"]]
+df = df.iterrows()
+
+#mycursor.execute("SELECT * FROM BettingData WHERE Time >= NOW() - INTERVAL 4 HOUR")
+
+#myresult = mycursor.fetchall()
+#print (myresult, type(myresult))
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=df)
 @app.route("/about")
 def about():
     return render_template("about.html")
