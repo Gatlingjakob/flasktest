4630f4b22cc0ae407558b517cebcfb710fdf73aa
Mikkel Jensen <mikkel465@live.dk>
2019-03-27 13:23:53 +0100

    Only displays matches from last 10 minutes

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+import mysql.connector
+
+mydb = mysql.connector.connect(
+	  host="localhost",
+	  user="root",
+	  passwd="",
+	  database="bets"
+	)
+
+mycursor = mydb.cursor()
+
+
+mycursor.execute("SELECT * FROM BettingData WHERE Time >= NOW() - INTERVAL 10 MINUTE")
+
+myresult = mycursor.fetchall()
+print myresult, type(myresult)
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=myresult)
 @app.route("/about")
 def about():
     return render_template("about.html")
